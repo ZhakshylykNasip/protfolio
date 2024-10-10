baf7484245b89a75773af1109eca42c51737aa5d
zhakshylyk <zhakshylyk@macs-MacBook-Pro.local>
2024-10-10 17:48:10 +0300

    HeroPage added

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,31 @@
+import { Html, useProgress } from "@react-three/drei";
 
-export const Loader = () => {
+const CanvasLoader = () => {
+  const { progress } = useProgress();
   return (
-    <div>Loader</div>
-  )
-}
+    <Html
+      as="div"
+      center
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <span className="canvas-load"></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: "#F1F1F1",
+          fontWeight: 800,
+          marginTop: 40,
+        }}
+      >
+        {progress.toFixed(2)}%
+      </p>
+    </Html>
+  );
+};
+
+export default CanvasLoader;
